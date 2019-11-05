9e8f3451ef6a8393a7225c7982073953c20b0164
Yash Garudkar <yashgarudkar@gmail.com>
2019-11-05 20:32:18 +0530

    Add files via upload

--- hill.cpp
+++ hill.cpp
@@ -20,8 +20,7 @@ int main(){
  int N;
  scanf("%d",&N);
  int arr[N];
- for(int i=0;i<N;i++)
-    scanf("%d",&arr[i]);
+ for(int i=0;i<N;i++) scanf("%d",&arr[i]);
  int bestCost=calcCost(arr,N),newCost,swaps=0;;
  while(bestCost>0){
   for(int i=0;i<N-1;i++){
@@ -41,3 +40,20 @@ int main(){
  printf("\n");
  return 0;
 }
+/*
+4                                                                                                                                      
+33                                                                                                                                     
+43                                                                                                                                     
+76                                                                                                                                     
+23                                                                                                                                     
+After swap 1:                                                                                                                          
+33 43 23 76                                                                                                                            
+After swap 2:                                                                                                                          
+33 23 43 76                                                                                                                            
+After swap 3:                                                                                                                          
+23 33 43 76                                                                                                                            
+Final Ans                                                                                                                              
+23 33 43 76  
+
+
+*/
